9b29c84fdc1263d43538b53a6047275a45386326
Mike Ball <mikedball@gmail.com>
2015-10-03 07:16:38 -0400

    add Today method

--- client_api.go
+++ client_api.go
@@ -1,5 +1,7 @@
 package seaweed
 
+import "time"
+
 func (c *Client) Forecast(spot string) ([]Forecast, error) {
 	forecasts := []Forecast{}
 	err := request(c, spotEp(c, spot), &forecasts)
@@ -8,4 +10,21 @@ func (c *Client) Forecast(spot string) ([]Forecast, error) {
 	}
 
 	return forecasts, nil
+}
+
+func (c *Client) Today(spot string) ([]Forecast, error) {
+	now := time.Now().Day()
+	today := []Forecast{}
+	forecasts, err := c.Forecast(spot)
+	if err != nil {
+		return today, err
+	}
+
+	for _, each := range forecasts {
+		if time.Unix(each.LocalTimestamp, 0).Day() == now {
+			today = append(today, each)
+		}
+	}
+
+	return today, nil
 }
